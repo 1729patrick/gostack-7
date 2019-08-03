58dec0c6c892dc0be6e6ba819fc2b225f6870b92
Patrick Battisti <1729patrick@gmail.com>
2019-08-03 20:01:52 -0300

    Mock userSelector

--- ReactJS/modulo12/src/components/TechList/index.js
+++ ReactJS/modulo12/src/components/TechList/index.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function TechList() {
-  const [techs, setTechs] = useState([]);
+  const dispatch = useDispatch();
+
+  const techs = useSelector(state => state.techs);
   const [newTech, setNewTech] = useState("");
 
-  useEffect(() => {
-    const techs = localStorage.getItem("techs");
-    if (techs) setTechs(JSON.parse(techs));
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("techs", JSON.stringify(techs));
-  }, [techs]);
-
   function handleAddTech() {
-    setTechs([...techs, newTech]);
+    dispatch({ type: "ADD_TECH", payload: { tech: newTech } });
     setNewTech("");
   }
 
