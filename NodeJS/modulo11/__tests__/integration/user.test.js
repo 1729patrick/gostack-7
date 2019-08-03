5b4fbad5c9cd96523d050b06ab043c7c3dc43490
Patrick Battisti <1729patrick@gmail.com>
2019-08-02 22:45:46 -0300

    E-mail duplicado

--- NodeJS/modulo11/__tests__/integration/user.test.js
+++ NodeJS/modulo11/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import trancate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await trancate();
+  });
+
   it('should be able register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Patrick Battisti',
+        email: '1729patrick@gmail.com',
+        password_hash: 123,
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Patrick Battisti',
+        email: '1729patrick@gmail.com',
+        password_hash: 123,
+      });
+
+    expect(response.status).toBe(400);
   });
 });
