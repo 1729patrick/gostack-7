265ec6f29e24240c5e0cc9cf7be9a1b44af55f54
Patrick Battisti <1729patrick@gmail.com>
2019-08-03 17:50:51 -0300

    Primeiro teste

--- ReactJS/modulo12/__tests__/App.test.js
+++
@@ -1,5 +0,0 @@
-import App from "~/App";
-
-test("example", () => {
-  expect(1 + 2).toBe(3);
-});
---
+++ ReactJS/modulo12/__tests__/components/TechList.test.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import { render, fireEvent } from "@testing-library/react";
+import TechList from "~/components/TechList";
+
+describe("TechList components", () => {
+  it("should be able to add new tech", () => {
+    const { getByText, getByTestId } = render(<TechList />);
+
+    fireEvent.click(getByText("Adicionar"));
+
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+  });
+});
