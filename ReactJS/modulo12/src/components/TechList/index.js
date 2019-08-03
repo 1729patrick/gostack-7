4dc29ca8b6d79da492bca0f2d8e84607323fdef9
Patrick Battisti <1729patrick@gmail.com>
2019-08-03 19:00:34 -0300

    Mock do localstorage

--- ReactJS/modulo12/src/components/TechList/index.js
+++ ReactJS/modulo12/src/components/TechList/index.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from "react";
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState("");
 
+  useEffect(() => {
+    const techs = localStorage.getItem("techs");
+    if (techs) setTechs(JSON.parse(techs));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("techs", JSON.stringify(techs));
+  }, [techs]);
+
   function handleAddTech() {
     setTechs([...techs, newTech]);
+    setNewTech("");
   }
+
   function handleNewTechChange(e) {
     setNewTech(e.target.value);
   }
@@ -23,7 +32,13 @@ export default function TechList() {
 
       <form onSubmit={handleAddTech} data-testid="tech-form">
         <label htmlFor="tech">Tech</label>
-        <input type="text" id="tech" onChange={handleNewTechChange} />
+        <input
+          type="text"
+          id="tech"
+          onChange={handleNewTechChange}
+          value={newTech}
+        />
+
         <button type="submit">Adicionar</button>
       </form>
     </div>
